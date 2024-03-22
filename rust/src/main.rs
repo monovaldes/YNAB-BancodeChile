7b93d376f0f9955e1697f1033eeb7d9ecdf704c9
Ivan Valdes Riesco <ivaldes@everlyhealth.com>
2024-03-22 19:59:11 -0300

    carga cartolas historicas

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,50 +2,77 @@ use calamine::{open_workbook, Reader, Xls};
 use chrono::NaiveDate;
 use csv::Writer;
 use std::error::Error;
-use std::path::Path;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-    let file_path = "../cartolas/cartola.xls";
-    let path = Path::new(file_path);
-    let mut reader: Xls<_> = open_workbook(path)?;
-    let r = match reader.worksheet_range("Hoja1") {
-        Ok(range) => range,
-        Err(err) => return Err(Box::new(err)),
-    };
-    let mut i = 0;
     let mut records: Vec<Vec<String>> = vec![];
     let mut writer = Writer::from_path("output.csv")?;
 
-    for row in r.rows() {
-        i += 1;
-        if i < 21 { continue; } // Skip headers
+    // Create Path vector for all xls files in cartolas directory
+    let paths = std::fs::read_dir("../cartolas")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, std::io::Error>>()?;
+    
+    // Iterate over each file in the cartolas directory
+    for path in paths {
+        // Skip if file name does not end in .xls
+        if path.file_name().unwrap().to_str().unwrap().chars().rev().take(4).collect::<String>() != "slx." {
+            continue;
+        }
 
-        // Skip empty rows following the transaction list
-        if row[0].is_empty() { break; }
+        println!("Processing file: {:?}", path);
 
-        let mut record: Vec<String> = vec![];
+        let path = path.as_path();
+        let mut reader: Xls<_> = open_workbook(path)?;
+        let r = match reader.worksheet_range("Hoja1") {
+            Ok(range) => range,
+            Err(err) => return Err(Box::new(err)),
+        };
+        let mut i = 0;
 
-        for (j, cell) in row.iter().enumerate() {
-            if j == 2 || j == 5 { continue; } // Skip columns 2 and 5
-            let cell = cell.to_string();
+        for row in r.rows() {
+            i += 1;
+            if i < 20 { continue; } // Skip headers
+    
+            // Skip empty rows following the transaction list
+            if row[0].is_empty() {
+                break;
+            }
+    
+            let mut record: Vec<String> = vec![];
+    
+            for (j, cell) in row.iter().enumerate() {
+                if j == 2 || j == 5 { continue; } // Skip columns 2 and 5
+                let cell = cell.to_string();
+                
+                if cell.contains("SALDO INICIAL") || cell.contains("SALDO FINAL") {
+                    continue;
+                }
+    
+                if j == 0 { // Parse date
+                    // get year from file name last 4 digits (excluding .xls extension), eg 1997.xls should return 1997
+                    let year = path.file_name().unwrap().to_str().unwrap().chars().rev().take(8).collect::<String>().chars().rev().collect::<String>();
+                    // remove .xls
+                    let year = year.chars().take(4).collect::<String>();
+                    // add year to cell
+                    let cell = format!("{}/{}", cell, year);
+                    let date = NaiveDate::parse_from_str(&cell, "%d/%m/%Y")?;
+                    record.push(date.to_string());
+                } else {
+                    record.push(cell);
+                }
+            }
+            // add empty string on column 2
+            record.insert(2, "".to_string());
 
-            if cell.contains("SALDO INICIAL") || cell.contains("SALDO FINAL") {
+            // skip push if second column is empty
+            if record[1].is_empty() {
                 continue;
             }
-
-            if j == 0 { // Parse date
-                let date = NaiveDate::parse_from_str(&cell, "%d/%m/%Y")?;
-                record.push(date.to_string());
-            } else {
-                record.push(cell);
-            }
+    
+            records.push(record);
         }
-        // add empty string on column 2
-        record.insert(2, "".to_string());
-
-        records.push(record);
     }
-
+    
     // Sort records by date
     records.sort_by(|a, b| a[0].cmp(&b[0]));
 
@@ -59,6 +86,9 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     for record in records {
         writer.write_record(&record)?;
     }
+
+    // Flush writer
+    println!("Flushing writer");
 
     writer.flush()?;
 
